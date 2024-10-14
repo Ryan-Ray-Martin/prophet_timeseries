e0a4e9c7a8aeb374190b0d329d228e2e0f5dbdc1
Ryan Martin <52725577+Ryan-Ray-Martin@users.noreply.github.com>
2024-10-13 18:12:11 -0700

    Delete .ipython directory

--- .ipython/profile_default/ipython_kernel_config.py
+++
@@ -1,9 +0,0 @@
-"""IPython configuration file."""
-
-# pylint: disable=undefined-variable
-c.IPKernelApp.extensions = [
-    'caip_notebooks_serverextension',
-    'google.cloud.bigquery',
-    'sql'
-]
-c.InteractiveShellApp.matplotlib = 'inline'
